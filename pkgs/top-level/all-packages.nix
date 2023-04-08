1bf952eba107f8f43eb4a8d4ab791860fed75de0
Tom McLaughlin <tom@codedown.io>
2023-04-07 18:15:01 -0700

    Working on julia-modules

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15774,6 +15774,8 @@ with pkgs;
   julia-stable-bin = julia_19-bin;
   julia-bin = julia-stable-bin;
 
+  juliaWithPackages = callPackage ../development/julia-modules { };
+
   jwasm =  callPackage ../development/compilers/jwasm { };
 
   kind2 = darwin.apple_sdk_11_0.callPackage ../development/compilers/kind2 { };
