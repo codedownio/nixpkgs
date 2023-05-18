698a00f5f80ab0c05203d5e432031b337fac3517
Tom McLaughlin <tom@codedown.io>
2023-05-18 04:05:00 -0600

    Improving indexing perf, using fork of SymbolServer.jl

--- pkgs/development/compilers/julia/default.nix
+++ pkgs/development/compilers/julia/default.nix
@@ -1,4 +1,5 @@
 { callPackage
+, fetchFromGitHub
 , runCommand
 }:
 
@@ -10,7 +11,19 @@ let
       withPackages = juliaWithPackages.override { inherit julia; };
 
       # For preindexing SymbolServer.jl symbols
-      symbolIndices = callPackage ../../julia-modules/indexing {};
+      symbolIndices = packageNames: callPackage ../../julia-modules/indexing {
+        julia = (juliaWithPackages.override {
+          packageOverrides = {
+            # Specially modified version of SymbolServer, which improved API and performance
+            "SymbolServer" = fetchFromGitHub {
+              owner = "codedownio";
+              repo = "SymbolServer.jl";
+              rev = "cfe81ec7830e6d8881191b3af7b35f143e4cd3eb";
+              sha256 = "YCefldfibbFl2TKwtvqNEwn1CFyCMWXWjZ47e6Nhh3w=";
+            };
+          };
+        }) (packageNames ++ ["SymbolServer"]);
+      };
       indexStdlib = runCommand "julia-index-stdlib" { buildInputs = [(juliaWithPackages ["SymbolServer"])]; } ''
         mkdir -p $out
         julia ${../../julia-modules/indexing/index-stdlib.jl}
