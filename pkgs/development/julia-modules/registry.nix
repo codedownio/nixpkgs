14e97590959e3bce7b88c8ca96c4f8fefd3afb0d
Tom McLaughlin <tom@codedown.io>
2025-05-09 02:43:42 -0700

    WIP

--- pkgs/development/julia-modules/registry.nix
+++ pkgs/development/julia-modules/registry.nix
@@ -3,7 +3,7 @@
 fetchFromGitHub {
   owner = "CodeDownIO";
   repo = "General";
-  rev = "998c6da1553dc0776dfff314d2f1bd5af488ed71";
-  sha256 = "sha256-57RiIPTu9895mdk3oSfo7I3PYw7G0BfJG1u+mYkJeLk=";
-  # date = "2024-07-01T12:22:35+00:00";
+  rev = "2d200892835adcc8e319edf0fdefb7c27e6d0b1f";
+  sha256 = "sha256-C4SMgFgpiVNJyHeDcUUw+BCkLGFiCiN2eihiyN12Y3c=";
+  # date = "2025-05-06T12:37:20Z";
 }
